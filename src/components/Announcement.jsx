96ff9f2a0224be04f2fde7d0495fb38884e1c749
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-10-30 11:09:19 +0100

    added first Components, installed packages

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from "./NavBar";
+
+const Announcement = () => {
+    return <Container>Great Shopping ! Free Transport on Payment over 50 euro</Container>
+}
+
+export default Announcement;
---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Announcement from "../components/Announcement";
+const Home = () => {
+    return (
+        <div>
+            <Announcement/>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default Home;
