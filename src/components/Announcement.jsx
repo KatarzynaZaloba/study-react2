8d2d7e5b6a5ab2c347b7860421757b1eda441cba
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-10-31 09:23:24 +0100

    added Footer to the React Controled Components, repaired import problem in Announcement

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-import Container from "./NavBar";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-height: 60px;
+  color: white;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
 
 const Announcement = () => {
     return <Container>Great Shopping ! Free Transport on Payment over 50 euro</Container>
