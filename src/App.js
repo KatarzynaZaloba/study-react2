7959b48ff3280786abb21e315ba03ec4894ea744
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-11-02 09:50:25 +0100

    added Cart to the React Controled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './pages/home';
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 function App() {
-  return <Login />
+  return <Cart />
 }
 
 export default App;
